566c64cbf151f90bfd6ca7c21948bace6662c3ed
Niko <nikolasm@outlook.de>
2021-11-19 13:17:12 +0100

    benutzersessions + passwortverschlüsselung

--- initialize_benutzer.sql
+++ initialize_benutzer.sql
@@ -3,8 +3,3 @@ CREATE TABLE benutzer (
     name TEXT NOT NULL,
     pass TEXT NOT NULL
 );
-
-INSERT INTO benutzer (name, pass) VALUES ("Alice", "§$Y45/912v");
-INSERT INTO benutzer (name, pass) VALUES ("Bob", "secret");
-INSERT INTO benutzer (name, pass) VALUES ("Carla", "123");
-INSERT INTO benutzer (name, pass) VALUES ("David", "divaD");
