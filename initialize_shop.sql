cee171d2b17fd8d0ccf00afe4fc709f95fe69a0c
Niko <nikolasm@outlook.de>
2022-01-07 17:24:39 +0100

    shop typo

--- initialize_shop.sql
+++ initialize_shop.sql
@@ -8,5 +8,5 @@ CREATE TABLE shop (
 INSERT INTO shop (name, preis, bild) VALUES ("White Widdow", 13.12, "White-Widdow.png");
 INSERT INTO shop (name, preis, bild) VALUES ("Pineapple Express", 14.99, "Pinappel-Express.png");
 INSERT INTO shop (name, preis, bild) VALUES ("Northern Light", 9.99, "Northern-Light.png");
-INSERT INTO shop (name, preis, bild) VALUES ("Girl Scout Cookes", 10.99, "Girl-Scout-Cookies.png");
+INSERT INTO shop (name, preis, bild) VALUES ("Girl Scout Cookies", 10.99, "Girl-Scout-Cookies.png");
 INSERT INTO shop (name, preis, bild) VALUES ("Cheese", 11.99, "Chees.png");
